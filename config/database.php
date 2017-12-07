a78204d1f2b23095a3cda2aac8fb5805f03766bb
mspanchenko <maxpanchnko@gmail.com>
2017-12-08 00:07:39 +0200

    kommit

--- config/database.php
+++ config/database.php
@@ -2,11 +2,6 @@
 
 $mysqlDriver = [
     'driver'      => 'mysql',
-    'host'        => env('DB_HOST', '127.0.0.1'),
-    'port'        => env('DB_PORT', '3306'),
-    'database'    => env('DB_DATABASE', 'forge'),
-    'username'    => env('DB_USERNAME', 'forge'),
-    'password'    => env('DB_PASSWORD', ''),
     'unix_socket' => env('DB_SOCKET', ''),
     'charset'     => 'utf8mb4',
     'collation'   => 'utf8mb4_unicode_ci',
@@ -47,55 +42,17 @@ $sqlSrv = [
 
 return [
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'default',
 
     'connections' => [
 
-        'sqlite' => [
-            'driver'   => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix'   => '',
-        ],
-
-        'mysql' => [
-            'driver'      => 'mysql',
-            'host'        => env('DB_HOST', '127.0.0.1'),
-            'port'        => env('DB_PORT', '3306'),
-            'database'    => env('DB_DATABASE', 'forge'),
-            'username'    => env('DB_USERNAME', 'forge'),
-            'password'    => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset'     => 'utf8mb4',
-            'collation'   => 'utf8mb4_unicode_ci',
-            'prefix'      => '',
-            'strict'      => true,
-            'engine'      => null,
-        ],
-
-        'pgsql' => [
-            'driver'   => 'pgsql',
+        'default' => array_merge($mysqlDriver, [
             'host'     => env('DB_HOST', '127.0.0.1'),
-            'port'     => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'port'     => env('DB_PORT', '3306'),
+            'database' => 'test',
+            'username' => 'test',
             'password' => env('DB_PASSWORD', ''),
-            'charset'  => 'utf8',
-            'prefix'   => '',
-            'schema'   => 'public',
-            'sslmode'  => 'prefer',
-        ],
-
-        'sqlsrv' => [
-            'driver'   => 'sqlsrv',
-            'host'     => env('DB_HOST', 'localhost'),
-            'port'     => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset'  => 'utf8',
-            'prefix'   => '',
-        ],
-
+        ]),
     ],
 
     'migrations' => 'migrations',
