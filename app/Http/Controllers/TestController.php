d1d9192c0b735b3bfb6dc25e06301ae52df615dd
mspanchenko <maxpanchnko@gmail.com>
2017-12-12 00:51:47 +0200

    test DI

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Test;
+
 class TestController extends Controller
 {
     //create service which get view or api json if request from site or platform
+    function __construct(Test $test)
+    {
+        echo '__construct';
+    }
+
+    public function test()
+    {
+        echo 'test';
+    }
 }
